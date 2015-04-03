e2ac94f9112743f4c3eefbd749fa2ae3b89d59a9
Thomas Buß <tbuss93@gmail.com>
2015-04-03 12:50:19 +0200

    created unpacking/min_max.py demonstrating multiple return values with Python functions

---
+++ aufgaben/unpacking/min_max.py
@@ -0,0 +1,25 @@
+__author__ = 'thomas'
+
+
+def min_max(l: list) -> tuple:
+    minv = l[0]
+    maxv = l[0]
+    for i in l:
+        if i < minv:
+            minv = i
+        if i > maxv:
+            maxv = i
+    return minv, maxv
+
+# Test und Aufruf
+
+liste = [3, 2, 1, 4, 5, 6, 9, 8, 7]
+(a, b) = min_max(liste)
+print("min:", a)
+print("max:", b)
+
+# Einen Wert annehmen
+c = min_max(liste)[0]
+
+# Werte tauschen
+a, b = b, a
