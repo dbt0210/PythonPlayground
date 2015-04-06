e193c252db42096839d3b37a8ae1e02480d1179d
AwesomeStryker <awesomestryker@gmail.com>
2015-04-06 21:17:53 +0200

    minor reformatting in math.py

--- aufgaben/math_sqrt/math.py
+++ aufgaben/math_sqrt/math.py
@@ -4,9 +4,9 @@ from math import factorial
 
 def guess_sqrt(number, iterations=5):
     x = number
-    for i in range(0,iterations):
+    for i in range(0, iterations):
         x = (x + number / x) / 2
-    return x, sqrt(number), x-sqrt(number)
+    return x, sqrt(number), x - sqrt(number)
 
 
 def fac_rec(number):
@@ -17,16 +17,16 @@ def fac_rec(number):
 
 def fac_iter(number):
     res = 1
-    for i in range(2, number+1):
-        res = res * i
+    for i in range(2, number + 1):
+        res *= i
     return res
 
 
 # Test
-print("sqrt(10) = " + guess_sqrt(10))
+print("sqrt(10) = " + str(guess_sqrt(10)))
 # this is quite fast
-print("42000! = " + factorial(42000))
+print("42000! = " + str(factorial(42000)))
 # this isn't
-print("42000! = " + fac_iter(42000))
+print("42000! = " + str(fac_iter(42000)))
 # Python's default maximum recursion depth is 1000.
 # print("42000! = " + fac_rec(42000))
