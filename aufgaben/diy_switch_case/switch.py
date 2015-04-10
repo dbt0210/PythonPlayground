29f00608cbc80392dd5ccf7350b241d4f7e5d014
dbt0210 <dbt0210@hotmail.de>
2015-04-10 20:55:12 +0200

    DIY switch-case: add comments and shorter version

--- aufgaben/diy_switch_case/switch.py
+++ aufgaben/diy_switch_case/switch.py
@@ -1,10 +1,20 @@
 from random import choice
 
 val = choice(range(1, 4))
+# so this is the basic idea:
+# create a dictionary mapping values to functions
 d = {
     1: lambda: print("Some option."),
     2: lambda: print("Some other option."),
+    # (use list comprehension to put multiple commands)
     3: lambda: [print("Interestingly, "), print("these are executed when you put them in a list")]
 }
+# call the function that is mapped to a specific value
 d[val]()
 
+# that's basically what happens here as well
+{
+    1: lambda: print("Some option."),
+    2: lambda: print("Some other option."),
+    3: lambda: [print("Interestingly, "), print("these are executed when you put them in a list")]
+}.get(val)()
