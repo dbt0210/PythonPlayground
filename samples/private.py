82ea65dfd1b82fbfcbbc6dba1a8c75862184f3f4
dbt0210 <dbt0210@hotmail.de>
2015-04-19 14:06:05 +0200

    Add example for private members

---
+++ samples/private.py
@@ -0,0 +1,13 @@
+class Foo:
+
+    def __init__(self, param):
+        self.__myprivateattribute = param
+
+    def __str__(self):
+        return str(self.__myprivateattribute)
+
+
+p = Foo(10)
+print(str(p)) # 10
+p._Person__myprivateattribute = 1
+print(str(p)) # 1
